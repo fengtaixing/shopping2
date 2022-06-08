fbe7cdf7ae4872fe956b0388e5d9536869bebe8e
11688 <847317487@qq.com>
2022-06-08 19:39:38 +0800

    分支111，4

--- shopping/src/main/java/com/itheima/B/Demo_B.java
+++ shopping/src/main/java/com/itheima/B/Demo_B.java
@@ -6,6 +6,10 @@ package com.itheima.B;
  */
 public class Demo_B {
     public static void main(String[] args) {
+        System.out.println("分支111，数据");
+    }
 
+    public  void test01() {
+        System.out.println("分支111，数据2222");
     }
 }
--- shopping/src/main/java/com/itheima/C/Demo_C.java
+++ shopping/src/main/java/com/itheima/C/Demo_C.java
@@ -6,6 +6,10 @@ package com.itheima.C;
  */
 public class Demo_C {
     public static void main(String[] args) {
+        System.out.println("分支7.15，数据");
+}
 
+    public  void test01() {
+        System.out.println("分支7.15，数据2222");
     }
 }
