05a74ea88f3520b1344aaf42284cfa53ffdf012f
11688 <847317487@qq.com>
2022-06-08 18:04:16 +0800

    重新提交

--- shopping/src/main/java/com/itheima/B/Demo_B.java
+++ shopping/src/main/java/com/itheima/B/Demo_B.java
@@ -6,11 +6,6 @@ package com.itheima.B;
  */
 public class Demo_B {
     public static void main(String[] args) {
-        //7.15
-        System.out.println("7.15");
-        System.out.println("7.15 修改");
-        System.out.println("7.15 修改了代码");
-        System.out.println("7.15 ,合并到7.18");
-        System.out.println("添加代码，6月8好");
+
 }
 }
