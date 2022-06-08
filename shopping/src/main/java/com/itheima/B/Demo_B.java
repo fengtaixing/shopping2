46d39952ddbbbf163e00147bc8b0f65850b109a7
11688 <847317487@qq.com>
2022-06-08 17:27:51 +0800

    11141

--- shopping/src/main/java/com/itheima/B/Demo_B.java
+++ shopping/src/main/java/com/itheima/B/Demo_B.java
@@ -11,6 +11,8 @@ public class Demo_B {
         System.out.println("7.15 修改");
         System.out.println("7.15 修改了代码");
         System.out.println("7.15 ,合并到7.18");
+        System.out.println("你打客服");
         System.out.println("添加代码，6月8好");
-}
+        System.out.println("女生发女生");
+    }
 }
