4c13914e40bdad2935a72645022c9dce318782f8
冯太星 <847317487@qq.com>
2022-06-08 17:22:53 +0800

    提交D，哈哈

--- shopping/src/main/java/com/itheima/B/Demo_B.java
+++ shopping/src/main/java/com/itheima/B/Demo_B.java
@@ -11,5 +11,6 @@ public class Demo_B {
         System.out.println("7.15 修改");
         System.out.println("7.15 修改了代码");
         System.out.println("7.15 ,合并到7.18");
-    }
+        System.out.println("添加代码，6月8好");
+}
 }
