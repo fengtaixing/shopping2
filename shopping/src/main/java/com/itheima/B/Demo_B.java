45d9e105e0dc56044216cf6592f0b6d4ec6fbbf1
11688 <847317487@qq.com>
2022-06-08 19:18:05 +0800

    分支111,1

--- shopping/src/main/java/com/itheima/B/Demo_B.java
+++ shopping/src/main/java/com/itheima/B/Demo_B.java
@@ -6,15 +6,11 @@ package com.itheima.B;
  */
 public class Demo_B {
     public static void main(String[] args) {
-        System.out.println("分支111,111");
-        System.out.println("分支111,222");
-        System.out.println("分支111,333");
-        System.out.println("==========================");
         System.out.println("分支111--");
         System.out.println("分支111==");
         System.out.println("分支111??");
     }
-    public  void test(){
-        System.out.println("分支111,都是废话");
+    public  void test01(){
+        System.out.println("分支111??");
     }
 }
--- shopping/src/main/java/com/itheima/C/Demo_C.java
+++ shopping/src/main/java/com/itheima/C/Demo_C.java
@@ -6,9 +6,6 @@ package com.itheima.C;
  */
 public class Demo_C {
     public static void main(String[] args) {
-        //7.18
-        System.out.println("C分支");
-        System.out.println("7.18");
         System.out.println("分支111,c");
     }
 
