c5268451237768d0ff5dad4122b70e22496abf95
11688 <847317487@qq.com>
2022-06-08 19:58:35 +0800

    分支7.15

--- shopping/src/main/java/com/itheima/B/Demo_B.java
+++ shopping/src/main/java/com/itheima/B/Demo_B.java
@@ -6,14 +6,10 @@ package com.itheima.B;
  */
 public class Demo_B {
     public static void main(String[] args) {
+        System.out.println("分支111，数据");
+    }
 
-        System.out.println("7.15,数据00b。。。");
-        System.out.println("分支111，数据b");
-    }
     public  void test01() {
-        System.out.println("7.15,数据00b。。。");
-    }
-    public  void test02() {
-        System.out.println("分支111，数据b");
+        System.out.println("分支111，数据2222");
     }
 }
