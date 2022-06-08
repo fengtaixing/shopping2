b680dfa06b034a067917794664986ea0a96b872d
11688 <847317487@qq.com>
2022-06-08 17:34:39 +0800

    提交123

--- shopping/src/main/java/com/itheima/B/Demo_B.java
+++ shopping/src/main/java/com/itheima/B/Demo_B.java
@@ -7,12 +7,7 @@ package com.itheima.B;
 public class Demo_B {
     public static void main(String[] args) {
         //7.15
-        System.out.println("7.15");
-        System.out.println("7.15 修改");
-        System.out.println("7.15 修改了代码");
-        System.out.println("7.15 ,合并到7.18");
-        System.out.println("你打客服");
-        System.out.println("添加代码，6月8好");
-        System.out.println("女生发女生");
+        System.out.println("你好");
+
     }
 }
