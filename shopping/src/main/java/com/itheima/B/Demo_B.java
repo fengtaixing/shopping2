cc458531c7ce6915c0c24179e0b255e9adda921b
11688 <847317487@qq.com>
2022-06-08 18:15:07 +0800

    分支111

--- shopping/src/main/java/com/itheima/B/Demo_B.java
+++ shopping/src/main/java/com/itheima/B/Demo_B.java
@@ -8,5 +8,7 @@ public class Demo_B {
     public static void main(String[] args) {
         System.out.println("不参加");
         System.out.println("不参加111");
+        System.out.println("分支111,222");
+        System.out.println("分支111,333");
     }
 }
