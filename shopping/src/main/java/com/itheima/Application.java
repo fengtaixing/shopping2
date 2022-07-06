82ef57a20c9b518dd70c455cd40d31f697f72db6
11688 <847317487@qq.com>
2022-07-06 22:39:00 +0800

    分支7.15,5

---
+++ shopping/src/main/java/com/itheima/Application.java
@@ -0,0 +1,14 @@
+package com.itheima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(Application.class);
+        application.addListeners(new ApplicationPidFileWriter());
+        application.run();
+    }
+}
--- shopping/src/main/java/com/itheima/B/Demo_B.java
+++ shopping/src/main/java/com/itheima/B/Demo_B.java
@@ -1,17 +1,27 @@
 package com.itheima.B;
 
+import com.itheima.C.Demo_C;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author taixing
  * @date 2021/7/18
  */
+@RestController
 public class Demo_B {
     public static void main(String[] args) {
         System.out.println("分支111，数据");
         System.out.println("分支111，数据");
     }
 
+    @Autowired
+    private Demo_C demo_c;
+
+    @GetMapping("test")
     public  void test01() {
         System.out.println("分支111，数据2222");
-        System.out.println("分支111，数据2222");
+        demo_c.test01();
     }
 }
